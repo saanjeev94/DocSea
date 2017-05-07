7685b7df3fc5f75d81c2a54d940cdbb298b60c74
bishal <monster_bishal@live.com>
2017-05-07 18:39:08 +0545

    server first commit

---
+++ server/src/main/java/org/itglance/docsea/DocseaApplication.java
@@ -0,0 +1,12 @@
+package org.itglance.docsea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocseaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocseaApplication.class, args);
+	}
+}
