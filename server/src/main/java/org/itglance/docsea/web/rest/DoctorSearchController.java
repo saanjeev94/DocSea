06e4e4fb5dda3d1d2e4b6b71227e69ab3bc56ba4
Mahesh Maharjan <mahesh.mrzn@gmail.com>
2017-05-25 17:29:05 +0545

    merged shreeyanka and mahesh for doctor search

--- server/src/main/java/org/itglance/docsea/web/rest/DoctorSearchController.java
+++ server/src/main/java/org/itglance/docsea/web/rest/DoctorSearchController.java
@@ -8,10 +8,7 @@ import org.itglance.docsea.service.dto.HospitalDoctorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +16,7 @@ import java.util.List;
  * Created by sriyanka on 5/25/2017.
  */
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/api/doctorSearch")
 public class DoctorSearchController {
